778cca251ba8ebf46115fb8f7ea770f41a7ed067
Alex Nowicki <al_nowicki@hotmail.com>
2025-05-12 08:53:54 -0300

    Initial keystatic local installation

---
+++ keystatic.config.ts
@@ -0,0 +1,21 @@
+// keystatic.config.ts
+import { config, fields, collection } from '@keystatic/core';
+
+export default config({
+  storage: {
+    kind: 'local',
+  },
+  collections: {
+    posts: collection({
+      label: 'Posts',
+      slugField: 'title',
+      path: 'src/content/posts/*',
+      format: { contentField: 'content' },
+      schema: {
+        title: fields.slug({ name: { label: 'Title' } }),
+        pubDate: fields.datetime({ label: 'Published Date' }),
+        content: fields.markdoc({ label: 'Content' }),
+      },
+    }),
+  },
+});
---
+++ src/content.config.ts
@@ -0,0 +1,19 @@
+// 1. Import utilities from `astro:content`
+import { defineCollection, z } from 'astro:content';
+
+// 2. Import loader(s)
+import { glob, file } from 'astro/loaders';
+
+// 3. Define your collection(s)
+const posts = defineCollection({ 
+    loader: glob({ pattern: "**/*.mdoc", base: "./src/content/posts" }),
+    schema: z.object({
+        title: z.string(),
+        // pubDate: z.date(),
+        // description: z.string(),
+        // author: z.string(),
+    })
+ });
+
+// 4. Export a single `collections` object to register your collection(s)
+export const collections = { posts };
