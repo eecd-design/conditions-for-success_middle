d675c11ba2022edbecfe13d649241e8a54489702
Alex Nowicki <al_nowicki@hotmail.com>
2026-07-23 13:18:11 -0300

    Updated formatting preferences

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,6 +1,11 @@
 // .prettierrc.mjs
 /** @type {import("prettier").Config} */
 export default {
+  useTabs: true,
+  tabWidth: 4,
+  singleQuote: true,
+  singleAttributePerLine: true,
+  printWidth: 100,
   plugins: ["prettier-plugin-astro"],
   overrides: [
     {
